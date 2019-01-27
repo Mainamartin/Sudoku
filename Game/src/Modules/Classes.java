4c5d09416dfc635e1c782f4d172357b93abed8f2
Martin <martinmaina@gmail.com>
2019-01-27 17:23:54 +0300

    Removed too much blank lines

--- Game/src/Modules/Classes.java
+++ Game/src/Modules/Classes.java
@@ -1,10 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Modules;
-
 
 import java.awt.Color;
 import java.awt.Font;
@@ -339,8 +333,3 @@ public class Classes {
 
 		
 	}
-	
-
-
-	
-
